534e7a02b926f2c5e9e65ded7176e086121b364d
Kostya Farber <kostya.farber@gmail.com>
2023-08-05 14:46:48 +0100

    add to github challenge, start xkcd challenge

--- gopl.io/ch4/ex11/main.go
+++ gopl.io/ch4/ex11/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -13,21 +14,50 @@ type Issues []struct {
 	Title  string
 }
 
+type createIssue struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+type createIssueResponse struct {
+	HtmlUrl string `json:"html_url"`
+}
+
 var (
 	bearerToken = os.Getenv("GITHUB_GO_TOKEN")
+	baseUrl     = "https://api.github.com/repos/kostyafarber/learning-go/%s"
 )
 
 func main() {
-	url := "https://api.github.com/repos/kostyafarber/learning-go/issues"
-	resp, err := http.Get(url)
+	c := http.Client{}
+	issues := fmt.Sprintf(baseUrl, "issues")
+	postBody := createIssue{
+		Title: "Creating a test post request, again...",
+		Body:  "It worked yay!",
+	}
+
+	postBodyJson, err := json.Marshal(postBody)
+	if err != nil {
+		log.Fatalf("Error making new marshals: %s", err)
+	}
+
+	post, err := http.NewRequest("POST", issues, bytes.NewBuffer(postBodyJson))
+	if err != nil {
+		log.Fatalf("Error making new request: %s", err)
+	}
+
+	post.Header.Add("Authorization", "Bearer "+bearerToken)
+	post.Header.Add("Accept", "application/vnd.github+json")
+
+	resp, err := c.Do(post)
 	if err != nil {
 		log.Fatalf("Error with get: %v", err)
 	}
 	defer resp.Body.Close()
 
-	var issues Issues
-	if err := json.NewDecoder(resp.Body).Decode(&issues); err != nil {
+	var issueResp createIssueResponse
+	if err := json.NewDecoder(resp.Body).Decode(&issueResp); err != nil {
 		log.Fatalf("Error with decoder: %v", err)
 	}
-	fmt.Printf("Title of the error is: %s\n", issues[0].Title)
+	fmt.Printf("Issue created! Url of the issue at: %s\n", issueResp.HtmlUrl)
 }
